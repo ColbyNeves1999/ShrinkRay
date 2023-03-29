7417b996ab40d120e21cf4c043d67f13f19d9262
Colby Neves <colby.neves@smail.astate.edu>
2023-03-29 09:37:31 -0500

    added .get for returning link to user

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './config';
 import 'express-async-errors';
 import express, { Express } from 'express';
 import { registerUser, login } from './controllers/UserController';
-import { shortenUrl, getOriginalUrl } from './controllers/LinkController';
+import { shortenUrl, getOriginalUrl, returningLinkToUser } from './controllers/LinkController';
 import session from 'express-session';
 import connectSqlite3 from 'connect-sqlite3';
 
@@ -12,13 +12,13 @@ const { PORT, COOKIE_SECRET } = process.env;
 const SQLiteStore = connectSqlite3(session);
 
 app.use(session({
-    store: new SQLiteStore({ db: 'sessions.sqlite', }),
-    secret: COOKIE_SECRET,
-    cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours 
-    name: 'session',
-    resave: false,
-    saveUninitialized: false,
-  }));
+  store: new SQLiteStore({ db: 'sessions.sqlite', }),
+  secret: COOKIE_SECRET,
+  cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours 
+  name: 'session',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use(express.json());
 
@@ -26,7 +26,8 @@ app.post('/api/users', registerUser);
 app.post('/api/login', login);
 app.post('/api/links', shortenUrl);
 app.get('/:targetLinkId', getOriginalUrl);
+app.get('/api/:targetLinkId/links', returningLinkToUser);
 
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
-  });
+  console.log(`Listening at http://localhost:${PORT}`);
+});
